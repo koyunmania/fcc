7740ebff4fe6a33c8ecec2ec77d32a87b6c6932d
koyunmania <vv1357@hotmail.com>
2016-01-01 22:10:18 +0200

    finished user input and frontend colors and design nok

--- fcc_tictactoe/custom.js
+++ fcc_tictactoe/custom.js
@@ -1,28 +1,62 @@
+var clickedCells = [ false, false, false,
+							false, false, false,
+							false, false, false];
+	
 $(document).ready( function(){
+
 	//Initialise game
 	var currentGame = new game([ 0,  0,  0,
 									0,  0,  0, 
 									0,  0,  0],-1, 0);
-	var player = "x"
+	var player = "x";
+	
+	// get user question
+	$(".select").fadeIn(500);
+	$(".answ_x").click(function( e ){
+		player = "x";
+		e.preventDefault();
+		$(".select").fadeOut(500);
+		//$(".select").attr("style", "display: none;");
+	});
+	$(".answ_o").click(function( e ){
+		player = "o";
+		e.preventDefault();
+		$(".select").fadeOut(500);
+		//$(".select").attr("style", "display: none;");
+	});
 	
 	//Workflow of main function
-	$("a").click( function(){
+	$(".cell").click( function( flag ){
 		//get which cell is clicked
 		var clickedCell = $(this).attr('class');
-		clickedCell = Number( clickedCell[ clickedCell.length - 1 ] );
+		clickedCell = Number( clickedCell[ clickedCell.length - 1 ] ) - 1;
 		
-		//Play user game and set move
-		var cloneBoard = currentGame.board.slice();
-		var clonePlayer = 1;
-		var cloneLast = clickedCell - 1;
-		cloneBoard[ clickedCell -1 ] = -1; 
-		var cloneStart = new game( cloneBoard, clonePlayer, cloneLast );
-		currentGame = cloneStart;
-		setMove( currentGame, player);
-		
-		//play computer game
-		currentGame = playComp( currentGame );
-		currentGame = setMove( currentGame, player );
+		if( !clickedCells[ clickedCell ] ) {
+			//marked the clicked cell as clicked
+			clickedCells[ clickedCell ] = true;
+	
+			//disable click function
+			$( this ).attr("style", "pointer-events: none;");
+			$( this ).attr("style", "cursor: default;");
+			
+			//Play user game and set move
+			var cloneBoard = currentGame.board.slice();
+			var clonePlayer = 1;
+			var cloneLast = clickedCell;
+			cloneBoard[ clickedCell ] = -1; 
+			var cloneStart = new game( cloneBoard, clonePlayer, cloneLast );
+			currentGame = cloneStart;
+			setMove( currentGame, player);
+			
+			//play computer game
+			currentGame = playComp( currentGame );
+			currentGame = setMove( currentGame, player );
+			
+			//set computer played cell inactive
+			clickedCells[ currentGame.last ] = true;
+			$( ".cella" + ( currentGame.last + 1 ) ).attr("style", "pointer-events: none;");
+			$( ".cella" + ( currentGame.last + 1 )  ).attr("style", "cursor: default;");
+		}
 	});	
 });
 
@@ -237,7 +271,6 @@ function playComp( start ){
 	// curring algorithm minimising looses
 	function bestMove( possibleMoves ){
 		var looseProbability = 1000000;
-		var winProbability = 0;
 		var winnerIndex = 0;
 		var winnerWins = 0;
 		var winnerLooses = 0;
@@ -304,422 +337,36 @@ function checkGameEnd( start ) {
 	switch( start.risk.riskIndex ) {
 		case -3:
 			alert( "you win" );
+			
+			//activate anchor links again
+			clickedCells.forEach( function( value ){
+				value = false;
+			});
+			$( ".cell" ).attr("style", "pointer-events: auto;");
+			$( ".cell" ).attr("style", "cursor: pointer;");
 			return 0;
 			break;
 		case 3:
 			alert( "you lose" );
+			
+			//activate anchor links again
+			clickedCells.forEach( function( value ){
+				value = false;
+			});
+			$( ".cell" ).attr("style", "pointer-events: auto;");
+			$( ".cell" ).attr("style", "cursor: pointer;");
 			return 0;
 			break;
 	}
 	if ( start.gameLevel === 0 ) {
 		alert( "it's a tie" );
+		
+		//activate anchor links again
+		clickedCells.forEach( function( value ){
+			value = false;
+		});
+		$( ".cell" ).attr("style", "pointer-events: auto;");
+		$( ".cell" ).attr("style", "cursor: pointer;");
 		return 0;
 	}
 }
-/*
-
-########### WORKING FUNC MODIFIED ##############
-
-function game( board, player, last ) {
-	this.board = board;
-	this.riskMat = [board[0] + board[1] + board[2],
-						board[3] + board[4] + board[5],
-						board[6] + board[7] + board[8],
-						board[0] + board[3] + board[6],
-						board[1] + board[4] + board[7],
-						board[2] + board[5] + board[8],
-						board[0] + board[4] + board[8],
-						board[2] + board[4] + board[6]];
-	this.player = player;
-	this.last = last;
-	this.risk = this.evaluateRisk( this.board );
-	this.gameLevel = this.evaluateLevel( this.board );
-}
-
-// riskindex  3 = player win
-// riskindex -3 = comp win
-// winRow = the index of the 3 in risk matrix (which row is finished)
-// riskindex 2 = player risk
-// riskCell = which cell should be played next because of danger
-// riskindex -2 = player risk
-// riskCell = which cell should be played next because of chanse to win
-// riskindex -3 = comp win
-// riskindex 0 = no risk, random play
-
-game.prototype.evaluateLevel = function( board ) {
-	var counter = 0;
-	board.forEach( function( cell ){
-		if( cell === 0 )
-			counter++;
-	});
-	return counter;
-}
-
-game.prototype.evaluateRisk = function( board ) {
-	var result = { riskIndex: 0, riskCell: 0, winRow: 0 };
-	var maxRisk = { riskIndex: 0, riskRow: 0 };
-	
-	for( var i = 0; i < this.riskMat.length; i++ ){
-		var risk = this.riskMat[i];
-		var index = i;
-		if ( Math.abs( risk ) === 3 ) {
-			maxRisk.riskIndex = risk;
-			maxRisk.riskRow = index;
-			break;
-		}
-		if ( Math.abs( risk ) === 2 ) {
-			if ( Math.abs( risk ) > maxRisk.riskIndex ) {
-				maxRisk.riskIndex = risk;
-				maxRisk.riskRow = index;
-			}
-		}
-	}
-	
-	if( maxRisk.riskIndex === 3 || maxRisk.riskIndex === -3 ) {
-		result.riskIndex = maxRisk.riskIndex;
-		result.riskCell = undefined;
-		result.winRow = maxRisk.riskRow;
-	}
-	else if( maxRisk.riskIndex === 2 || maxRisk.riskIndex === -2 ) {
-		result.riskIndex = maxRisk.riskIndex;
-		switch( maxRisk.riskRow ) {
-			case 0:
-				var arr = [ 0, 1, 2];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 1:
-				var arr = [ 3, 4, 5];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 2:
-				var arr = [ 6, 7, 8];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 3:
-				var arr = [ 0, 3, 6];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 4:
-				var arr = [ 1, 4, 7];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 5:
-				var arr = [ 2, 5, 8];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 6:
-				var arr = [ 0, 4, 8];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 7:
-				var arr = [ 2, 4, 6];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-		}
-	}
-	else {
-		result.riskIndex = 0;
-		result.riskCell = undefined;
-		result.winRow = undefined;
-	}
-	return result;
-}
-
-//						0	1	2
-//						3	4	5
-//						6	7	8
-var start = new game(	[ 1,  0,  0,
-						 -1, -1,  1, 
-						  0,  0,  0], -1, 4);
-var result = { wins: 0, looses: 0 };
-
-function getAllPossibileWins( gameObj ) {
-	// check if game end
-	if( gameObj.risk.riskIndex === 3  ) {
-		result.wins++ ;
-		return "win";
-	}
-	else if( gameObj.risk.riskIndex === -3  ) {
-		result.looses++;
-		return "loose";
-	}
-	else if ( gameObj.gameLevel === 0 )
-		return "tie";
-		
-	for ( var i = 0; i < gameObj.board.length; i++ ) {
-		if ( gameObj.board[i] === 0 ) {
-			var tempBoard = gameObj.board.slice();
-			var tempPlayer = gameObj.player*(-1);
-			var tempLast = i;
-			tempBoard[i] = tempPlayer
-			var cloneGame = new game( tempBoard, tempPlayer, tempLast );
-			getAllPossibileWins( cloneGame );
-		}
-	}
-}
-
-function getPossibleMoves( gameObj ){
-	var moves = [];
-	for ( var i = 0; i < gameObj.board.length; i++ ) {
-		if ( gameObj.board[i] === 0 ) {
-			var tempBoard = gameObj.board.slice();
-			var tempPlayer = gameObj.player*(-1);
-			var tempLast = i;
-			tempBoard[i] = tempPlayer
-			var cloneGame = new game( tempBoard, tempPlayer, tempLast );
-			moves.push( cloneGame );
-		}
-	}
-	return moves;
-}
-
-function bestMove( possibleMoves ){
-	winProbability = 0;
-	winnerIndex = 0;
-	possibleMoves.forEach( function( possibleMove, index){
-		getAllPossibileWins( possibleMove );
-		if( result.wins / result.looses > winProbability ) {
-			winProbability = result.wins / result.looses;
-			winnerIndex = index;
-		}
-		console.log( "wins: " + result.wins + "	Losts: " + result.looses );
-		console.log( possibleMove.board[0]  + "	" + possibleMove.board[1]  + "	" + possibleMove.board[2]);
-		console.log( possibleMove.board[3]  + "	" + possibleMove.board[4]  + "	" + possibleMove.board[5]);
-		console.log( possibleMove.board[6]  + "	" + possibleMove.board[7]  + "	" + possibleMove.board[8]);
-		result.wins = 0;
-		result.looses = 0;
-	});
-	console.log( "######################" )
-	console.log( "######## FINAL #######" )
-	console.log( "######################" )
-	console.log( "wins: " + result.wins + "	Losts: " + result.looses );
-	console.log( possibleMoves[ winnerIndex ].board[0]  + "	" + possibleMoves[ winnerIndex ].board[1]  + "	" + possibleMoves[ winnerIndex ].board[2]);
-	console.log( possibleMoves[ winnerIndex ].board[3]  + "	" + possibleMoves[ winnerIndex ].board[4]  + "	" + possibleMoves[ winnerIndex ].board[5]);
-	console.log( possibleMoves[ winnerIndex ].board[6]  + "	" + possibleMoves[ winnerIndex ].board[7]  + "	" + possibleMoves[ winnerIndex ].board[8]);
-}
-
-bestMove( getPossibleMoves( start ) );
-console.log( "end" );
-
-
-########## WORKING FUNC ORIGINAL ###############
-
-function game( board, player, last ) {
-	this.board = board;
-	this.riskMat = [board[0] + board[1] + board[2],
-						board[3] + board[4] + board[5],
-						board[6] + board[7] + board[8],
-						board[0] + board[3] + board[6],
-						board[1] + board[4] + board[7],
-						board[2] + board[5] + board[8],
-						board[0] + board[4] + board[8],
-						board[2] + board[4] + board[6]];
-	this.player = player;
-	this.last = last;
-	this.risk = this.evaluateRisk( this.board );
-	this.gameLevel = this.evaluateLevel( this.board );
-}
-
-// riskindex  3 = player win
-// riskindex -3 = comp win
-// winRow = the index of the 3 in risk matrix (which row is finished)
-// riskindex 2 = player risk
-// riskCell = which cell should be played next because of danger
-// riskindex -2 = player risk
-// riskCell = which cell should be played next because of chanse to win
-// riskindex -3 = comp win
-// riskindex 0 = no risk, random play
-
-game.prototype.evaluateLevel = function( board ) {
-	var counter = 0;
-	board.forEach( function( cell ){
-		if( cell === 0 )
-			counter++;
-	});
-	return counter;
-}
-
-game.prototype.evaluateRisk = function( board ) {
-	var result = { riskIndex: 0, riskCell: 0, winRow: 0 };
-	var maxRisk = { riskIndex: 0, riskRow: 0 };
-	
-	for( var i = 0; i < this.riskMat.length; i++ ){
-		var risk = this.riskMat[i];
-		var index = i;
-		if ( Math.abs( risk ) === 3 ) {
-			maxRisk.riskIndex = risk;
-			maxRisk.riskRow = index;
-			break;
-		}
-		if ( Math.abs( risk ) === 2 ) {
-			if ( Math.abs( risk ) > maxRisk.riskIndex ) {
-				maxRisk.riskIndex = risk;
-				maxRisk.riskRow = index;
-			}
-		}
-	}
-	
-	if( maxRisk.riskIndex === 3 || maxRisk.riskIndex === -3 ) {
-		result.riskIndex = maxRisk.riskIndex;
-		result.riskCell = undefined;
-		result.winRow = maxRisk.riskRow;
-	}
-	else if( maxRisk.riskIndex === 2 || maxRisk.riskIndex === -2 ) {
-		result.riskIndex = maxRisk.riskIndex;
-		switch( maxRisk.riskRow ) {
-			case 0:
-				var arr = [ 0, 1, 2];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 1:
-				var arr = [ 3, 4, 5];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 2:
-				var arr = [ 6, 7, 8];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 3:
-				var arr = [ 0, 3, 6];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 4:
-				var arr = [ 1, 4, 7];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 5:
-				var arr = [ 2, 5, 8];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 6:
-				var arr = [ 0, 4, 8];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-			case 7:
-				var arr = [ 2, 4, 6];
-				arr.forEach( function(val){
-					if( board[val] === 0)
-						result.riskCell = val;
-					});
-				break;
-		}
-	}
-	else {
-		result.riskIndex = 0;
-		result.riskCell = undefined;
-		result.winRow = undefined;
-	}
-	return result;
-}
-
-//						0	1	2
-//						3	4	5
-//						6	7	8
-var start = new game(	[ 1,  0,  0,
-						 -1, -1,  1, 
-						  0,  0,  0], -1, 4);
-var result = { wins: 0, looses: 0 };
-
-function getAllPossibileWins( gameObj ) {
-	// check if game end
-	if( gameObj.risk.riskIndex === 3  ) {
-		result.wins++ ;
-		return "win";
-	}
-	else if( gameObj.risk.riskIndex === -3  ) {
-		result.looses++;
-		return "loose";
-	}
-	else if ( gameObj.gameLevel === 0 )
-		return "tie";
-		
-	for ( var i = 0; i < gameObj.board.length; i++ ) {
-		if ( gameObj.board[i] === 0 ) {
-			var tempBoard = gameObj.board.slice();
-			var tempPlayer = gameObj.player*(-1);
-			var tempLast = i;
-			tempBoard[i] = tempPlayer
-			var cloneGame = new game( tempBoard, tempPlayer, tempLast );
-			getAllPossibileWins( cloneGame );
-		}
-	}
-}
-
-function getPossibleMoves( gameObj ){
-	var moves = [];
-	for ( var i = 0; i < gameObj.board.length; i++ ) {
-		if ( gameObj.board[i] === 0 ) {
-			var tempBoard = gameObj.board.slice();
-			var tempPlayer = gameObj.player*(-1);
-			var tempLast = i;
-			tempBoard[i] = tempPlayer
-			var cloneGame = new game( tempBoard, tempPlayer, tempLast );
-			moves.push( cloneGame );
-		}
-	}
-	return moves;
-}
-
-function bestMove( possibleMoves ){
-	winnerMove = 0;
-	winnerIndex = 0;
-	possibleMoves.forEach( function( possibleMove, index){
-		getAllPossibileWins( possibleMove );
-		console.log( "wins: " + result.wins + "	Losts: " + result.looses );
-		console.log( possibleMove.board[0]  + "	" + possibleMove.board[1]  + "	" + possibleMove.board[2]);
-		console.log( possibleMove.board[3]  + "	" + possibleMove.board[4]  + "	" + possibleMove.board[5]);
-		console.log( possibleMove.board[6]  + "	" + possibleMove.board[7]  + "	" + possibleMove.board[8]);
-		result.wins = 0;
-		result.looses = 0;
-	});
-}
-
-bestMove( getPossibleMoves( start ) );
-console.log( "end" );
-
-*/
